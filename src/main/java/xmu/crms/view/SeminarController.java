e7d3b77d22148a166c03237fbd155b9623f428a0
WCLHLWLWLKQHR <847156155@qq.com>
2018-01-05 20:21:06 +0800

    wc

--- src/main/java/xmu/crms/view/SeminarController.java
+++ src/main/java/xmu/crms/view/SeminarController.java
@@ -248,10 +248,11 @@ public class SeminarController {
 			try {
 				List<SeminarGroup> seminarGroups = seminarGroupService.listSeminarGroupBySeminarId(BigInteger.valueOf(seminarId));
 				for (SeminarGroup seminarGroup : seminarGroups) {
-					System.out.println(seminarGroup);
 					if (seminarGroup.getClassInfo().getId().intValue() == classId) {
 						List<Topic> topicList = topicService.listTopicBySeminarId(seminarGroup.getId());
-						seminarGroupTopicsVOList.add(new SeminarGroupTopicsVO(seminarGroup, topicList));
+						//System.out.println(topicList);
+						SeminarGroupTopicsVO seminarGroupTopicsVO = new SeminarGroupTopicsVO(seminarGroup, topicList);
+						seminarGroupTopicsVOList.add(seminarGroupTopicsVO);
 					}
 				}
 				return ResponseEntity.status(200).contentType(MediaType.APPLICATION_JSON_UTF8).body(seminarGroupTopicsVOList);
--- src/main/java/xmu/crms/view/vo/SeminarTopicVO.java
+++ src/main/java/xmu/crms/view/vo/SeminarTopicVO.java
@@ -25,4 +25,12 @@ public class SeminarTopicVO {
     public void setName(String name) {
         this.name = name;
     }
+
+    @Override
+    public String toString() {
+        return "SeminarTopicVO{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
